93cf0d7487be5a24a360a5e7f19230489b1c283c
Pierre Bonet <pierotibou@gmail.com>
2022-04-26 14:22:46 +0200

    Update Program.cs

--- tracer/tools/PipelineMonitor/Program.cs
+++ tracer/tools/PipelineMonitor/Program.cs
@@ -63,7 +63,8 @@ try
 {
     var processor = new BuildProcessor(buildData, timeline);
     processor.Process();
-    Console.WriteLine("Build " + buildId + " has been processed successfully. It should be visible at: https://ddstaging.datadoghq.com/apm/services/consolidated-pipeline/operations/ci_run/resources");
+    Console.WriteLine("Build " + buildId + " has been processed successfully. It should be visible at: https://dd-dotnet.datadoghq.com/apm/services/consolidated-pipeline/operations/ci_run/resources");
+    await Tracer.Instance.ForceFlushAsync();
 }
 catch (Exception ex)
 {
